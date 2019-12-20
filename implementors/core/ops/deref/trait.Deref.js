7cd6df4370964139771b1db3231a39dd3bfa49cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-20 21:22:42 +0000

    Deploy edomora97/task-maker-rust to github.com/edomora97/task-maker-rust.git:gh-pages

--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["task_maker_exec"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"task_maker_exec/struct.ChannelServer.html\" title=\"struct task_maker_exec::ChannelServer\">ChannelServer</a>",synthetic:false,types:["task_maker_exec::ChannelServer"]},];
+implementors["task_maker_exec"] = [{text:"impl&lt;S, R&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"task_maker_exec/struct.ChannelServer.html\" title=\"struct task_maker_exec::ChannelServer\">ChannelServer</a>&lt;S, R&gt;",synthetic:false,types:["task_maker_exec::ChannelServer"]},];
 implementors["task_maker_format"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"task_maker_format/struct.SourceFile.html\" title=\"struct task_maker_format::SourceFile\">SourceFile</a>",synthetic:false,types:["task_maker_format::source_file::SourceFile"]},];
 
             if (window.register_implementors) {
