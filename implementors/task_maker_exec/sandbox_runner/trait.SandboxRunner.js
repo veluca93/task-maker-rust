3118038b501c80d324840149cf770baf4c387b95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-12 17:28:59 +0000

    Deploy edomora97/task-maker-rust to github.com/edomora97/task-maker-rust.git:gh-pages

---
+++ implementors/task_maker_exec/sandbox_runner/trait.SandboxRunner.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["task_maker"] = [{"text":"impl <a class=\"trait\" href=\"task_maker_exec/sandbox_runner/trait.SandboxRunner.html\" title=\"trait task_maker_exec::sandbox_runner::SandboxRunner\">SandboxRunner</a> for <a class=\"struct\" href=\"task_maker/sandbox/struct.SelfExecSandboxRunner.html\" title=\"struct task_maker::sandbox::SelfExecSandboxRunner\">SelfExecSandboxRunner</a>","synthetic":false,"types":["task_maker::sandbox::SelfExecSandboxRunner"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
--- source-files.js
+++ source-files.js
@@ -2,7 +2,7 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["task_maker"] = {"name":"","files":["error.rs","local.rs","main.rs","opt.rs","sandbox.rs","server.rs","worker.rs"]};
 sourcesIndex["task_maker_cache"] = {"name":"","files":["entry.rs","key.rs","lib.rs"]};
 sourcesIndex["task_maker_dag"] = {"name":"","files":["dag.rs","execution.rs","file.rs","lib.rs","signals.rs"]};
-sourcesIndex["task_maker_exec"] = {"name":"","dirs":[{"name":"executors","files":["local_executor.rs","mod.rs","remote_executor.rs"]}],"files":["check_dag.rs","client.rs","executor.rs","lib.rs","proto.rs","sandbox.rs","scheduler.rs","worker.rs","worker_manager.rs"]};
+sourcesIndex["task_maker_exec"] = {"name":"","dirs":[{"name":"executors","files":["local_executor.rs","mod.rs","remote_executor.rs"]}],"files":["check_dag.rs","client.rs","executor.rs","lib.rs","proto.rs","sandbox.rs","sandbox_runner.rs","scheduler.rs","worker.rs","worker_manager.rs"]};
 sourcesIndex["task_maker_format"] = {"name":"","dirs":[{"name":"ioi","dirs":[{"name":"format","dirs":[{"name":"italian_yaml","files":["gen_gen.rs","mod.rs","static_inputs.rs"]}],"files":["mod.rs"]},{"name":"statement","files":["asy.rs","booklet.rs","mod.rs","statement.rs"]}],"files":["curses_ui.rs","dag.rs","finish_ui.rs","mod.rs","print.rs","sanity_checks.rs","tag.rs","ui_state.rs"]},{"name":"ui","files":["json.rs","mod.rs","raw.rs","silent.rs"]}],"files":["lib.rs","source_file.rs"]};
 sourcesIndex["task_maker_lang"] = {"name":"","dirs":[{"name":"languages","files":["c.rs","cpp.rs","mod.rs","pascal.rs","python.rs","shell.rs"]}],"files":["grader_map.rs","lib.rs","source_file.rs"]};
 sourcesIndex["task_maker_rust"] = {"name":"","files":["error.rs","lib.rs","local.rs","opt.rs","sandbox.rs","server.rs","worker.rs"]};
