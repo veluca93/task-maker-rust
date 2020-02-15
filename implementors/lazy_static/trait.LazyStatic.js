5f933400238e141d895c6a68d309ece0e8d89c93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-15 18:20:12 +0000

    Deploy edomora97/task-maker-rust to github.com/edomora97/task-maker-rust.git:gh-pages

---
+++ implementors/lazy_static/trait.LazyStatic.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["task_maker_format"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/lazy_static/1.4.0/lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"task_maker_format/ioi/struct.VALID_TAGS.html\" title=\"struct task_maker_format::ioi::VALID_TAGS\">VALID_TAGS</a>","synthetic":false,"types":["task_maker_format::ioi::tag::VALID_TAGS"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
