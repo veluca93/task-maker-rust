e7ae5f38e0fdf0c3cdc93630983241806d4f663b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-07 13:13:08 +0000

    Deploy edomora97/task-maker-rust to github.com/edomora97/task-maker-rust.git:gh-pages

--- implementors/askama/trait.Template.js
+++ implementors/askama/trait.Template.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["task_maker_format"] = [{"text":"impl Template for <a class=\"struct\" href=\"task_maker_format/ioi/struct.BookletTemplate.html\" title=\"struct task_maker_format::ioi::BookletTemplate\">BookletTemplate</a>","synthetic":false,"types":["task_maker_format::ioi::statement::booklet::BookletTemplate"]}];
+implementors["task_maker_format"] = [{"text":"impl Template for BookletTemplate","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
