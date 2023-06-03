df18ed1c9230f04319bd67632b42b626115bdc33
veluca93 <veluca93@gmail.com>
2023-06-03 10:11:43 +0000

    Deploying to gh-pages from @ veluca93/task-maker-rust@5b2eab298095e5c1eb51018b99c3dd51b9bddeda 🚀

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"task_maker_dag":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://docs.rs/uuid/1.3.3/uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>&gt; for <a class=\"struct\" href=\"task_maker_dag/struct.File.html\" title=\"struct task_maker_dag::File\">File</a>"]]
+"task_maker_dag":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://docs.rs/uuid/1.3.3/uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>&gt; for <a class=\"struct\" href=\"task_maker_dag/struct.File.html\" title=\"struct task_maker_dag::File\">File</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
